d4cd92986f8072c9e1849f15a79c24c72515a69d
Colin McDonald <colinmcd0731@gmail.com>
2026-04-14 02:00:23 -0400

    Tried to remove a volatile qualifier, but it introduced a bug

--- parfor.hpp
+++ parfor.hpp
@@ -2,7 +2,10 @@
 #define SPORK_PARFOR_H_
 
 #include "parlay/monoid.h"
+#include "parlay/parallel.h"
+#include "parlay/portability.h"
 #include "scheduler.hpp"
+#include <atomic>
 #include <bits/types/sig_atomic_t.h>
 #include <limits.h>
 
@@ -36,16 +39,26 @@ void seqfor(idx n, const BodyLambda&& body) {
 }
 
 
+namespace { // private
 template <typename idx>
 __attribute__((always_inline))
 constexpr static const idx midpoint(idx i, idx j) noexcept {
   static_assert(std::is_integral_v<idx>);
   return i + ((j - i) / 2);
 }
+} // private
+
+// template<typename T>
+// struct plus : public std::plus<> {
+//   static_assert(is_binary_operator_for_v<std::plus<>, T>);
+//   T identity;
+//   plus() : identity(0) { }
+//   explicit plus(T identity_) : identity(std::move(identity_)) { }
+// };
 
 template <typename idx, typename BodyLambda, typename BinOp>
 //__attribute__((always_inline)) // TODO: investigate what this attribute actually does
-parlay::monoid_value_type_t<BinOp> parfor(idx _i, idx _j, const BodyLambda&& body, const BinOp&& binop) {
+parlay::monoid_value_type_t<BinOp> parfor(idx i, idx j, const BodyLambda&& body, const BinOp&& binop) {
   static_assert(std::is_integral_v<idx>);
   static_assert(parlay::is_monoid_v<BinOp>);
   using A = parlay::monoid_value_type_t<BinOp>;
@@ -70,102 +83,74 @@ parlay::monoid_value_type_t<BinOp> parfor(idx _i, idx _j, const BodyLambda&& bod
 
   // make sure that loop index can fit in `i`
   static_assert(sizeof(sig_atomic_t) >= sizeof(idx));
-  volatile sig_atomic_t i = _i;
-  volatile idx j = _j;
-  A a = binop.identity;
+  // static_assert(std::numeric_limits<sig_atomic_t>::max() >= std::numeric_limits<idx>::max());
+  // TODO: do we need to use a `std::atomic_signal_fence` anywhere?
+  // https://en.cppreference.com/w/cpp/atomic/atomic_signal_fence.html
+
+  // main code may write `sig_safe_i`; signal handler may only read
+  volatile sig_atomic_t sig_safe_i = i;
+  // main code may only read `loop_end`; signal handler may write
+  // __attribute__((, 12345));
+  volatile idx loop_end = j;
+  A a = fwd(binop).identity;
 
   bool promoted = with_prom_handler(
     [&] () {
-      for (idx ii = _i; ii < j;) {
-        fwd(body)(ii, a);
-        i = ++ii;
+      for (; i < loop_end;) {
+        fwd(body)(i, a);
+        //std::atomic_signal_fence(std::memory_order_acquire);
+        sig_safe_i = static_cast<sig_atomic_t>(++i);
+        //std::atomic_signal_fence(std::memory_order_release);
       }
     },
     [&] () {
-      idx _i = i + 1;
-      if (_i >= _j) { r.i = 0; r.j = 0; l.i = 0; l.j = 0; return; }
-      idx mid = midpoint<idx>(_i, _j);
-      j = _i;
+      idx prom_i = sig_safe_i + 1;
+      if (prom_i >= loop_end) { r.i = 0; r.j = 0; l.i = 0; l.j = 0; return; }
+      idx mid = midpoint<idx>(prom_i, loop_end);
+      loop_end = prom_i;
 
       r.i = mid;
-      r.j = _j;
+      r.j = j;
       r.enqueue((heartbeat_tokens + 1) >> 1);
 
       // TODO: check if work stealing deque is full before enqueueing
 
-      if (_i >= mid) { l.i = 0; l.j = 0; return; }
-      l.i = _i;
+      if (prom_i >= mid) { l.i = 0; l.j = 0; return; }
+      l.i = prom_i;
       l.j = mid;
       l.enqueue(heartbeat_tokens);
     });
   if (promoted) [[unlikely]] {
     if (l.i < l.j) [[likely]] {
       l.sync(true);
-      a = binop(a, l.a);
+      a = fwd(binop)(a, l.a);
     }
     if (r.i < r.j) [[likely]] {
       r.sync(false);
-      a = binop(a, r.a);
+      a = fwd(binop)(a, r.a);
     }
   }
   return a;
 }
 
+// TODO
+// struct Unit {
+//   Unit() {}
+//   Unit(Unit& other) {}
+//   Unit(Unit&& other) {}
+//   Unit& operator=(Unit& other) { return *this; }
+//   Unit& operator=(Unit&& other) { return *this; }
+// };
+
+//#include <__functional/binary_function.h>
 
 template <typename idx, typename BodyLambda>
 //__attribute__((always_inline)) // TODO: investigate what this attribute actually does
-void parfor(idx _i, idx _j, const BodyLambda&& body) {
-  static_assert(std::is_integral_v<idx>);
-  static_assert(std::is_invocable_r_v<void, BodyLambda&, idx>);
+void parfor(idx i, idx j, const BodyLambda&& body) {
+  // TODO
+  // parfor<idx>(_i, _j, [&] (idx i, Unit& u) {fwd(body)(i);}, fwd(u));
 
-  struct SpwnJob : WorkStealingJob {
-    idx i, j;
-    const BodyLambda&& body;
-    void run() override {
-      parfor(i, j, fwd(body));
-    }
-    SpwnJob(const BodyLambda&& _body) :
-      WorkStealingJob(),
-      body(fwd(_body)) {}
-  };
-
-  SpwnJob l(fwd(body));
-  SpwnJob r(fwd(body));
-
-  // make sure that loop index can fit in `i`
-  static_assert(sizeof(sig_atomic_t) >= sizeof(idx));
-  volatile sig_atomic_t i = _i;
-  
-  volatile idx j = _j;
-
-  bool promoted = with_prom_handler(
-    [&] () {
-      for (idx ii = _i; ii < j;) {
-        fwd(body)(ii);
-        i = ++ii;
-      }
-    },
-    [&] () {
-      idx _i = i + 1;
-      if (_i >= _j) { r.i = 0; r.j = 0; l.i = 0; l.j = 0; return; }
-      idx mid = midpoint<idx>(_i, _j);
-      j = _i;
-
-      r.i = mid;
-      r.j = _j;
-      r.enqueue((heartbeat_tokens + 1) >> 1);
-
-      // TODO: check if work stealing deque is full before enqueueing
-
-      if (_i >= mid) { l.i = 0; l.j = 0; return; }
-      l.i = _i;
-      l.j = mid;
-      l.enqueue(heartbeat_tokens);
-    });
-  if (promoted) [[unlikely]] {
-    if (l.i < l.j) [[likely]] l.sync(true);
-    if (r.i < r.j) [[likely]] r.sync(false);
-  }
+  char _ = parfor(i, j, [&] (idx i, char _) {fwd(body)(i);}, parlay::plus<char>());
 }
 
 template <typename idx, typename BodyLambda>

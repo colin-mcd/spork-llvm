d50ab0ab54f7eea17eceaf82a71edcfa3927fae1
Colin McDonald <colinmcd0731@gmail.com>
2026-04-29 00:40:55 -0400

    Add sum example

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+#include "scheduler.hpp"
+#include "parlay/primitives.h"
+#include "parfor.hpp"
+#include "benchmark.hpp"
+
+int main(int argc, char* argv[]) {
+  int n;
+  if (argc == 3 && strcmp(argv[1], "-n") == 0) {
+    n = atoi(argv[2]);
+  } else {
+    n = 800000000;
+  }
+
+  using idxnum = unsigned;
+  using datnum = short;
+  parlay::sequence<datnum> data = parlay::tabulate(n, [&] (long k) -> datnum { return 1; });
+  benchmark([n, &data] () {
+    return spork::seqfor(n, [&] (idxnum i, datnum& total) {
+      total += data[i];
+    }, parlay::plus<datnum>());
+  });
+}
